10e113c90902966952a852824d305b86907178d2
samshals19 <56256306+samshals19@users.noreply.github.com>
2022-08-28 22:50:52 +0530

    tillpage4done

--- Desktop/reactjs/arbor/arswp/src/App.js
+++ Desktop/reactjs/arbor/arswp/src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import App1 from "./App1";
 import App2 from "./App2";
+import App3 from "./App3";
+import App4 from "./App4";
 
 function App() {
   return (
     <>
       <div className="App">
         {/* <App1 /> */}
-        <App2 />
+        {/* <App2 /> */}
+        {/* <App3 /> */}
+        <App4 />
       </div>
     </>
   );
--- Desktop/reactjs/arbor/arswp/src/App3.js
+++ Desktop/reactjs/arbor/arswp/src/App3.js
@@ -261,59 +261,60 @@ class Home extends React.Component {
           </div>
 
           {/* content2 */}
-          <div className="contain2 fl-right">
-            <div className="contain2_section1 clear">
-              <div className="contain2_section1_sub1 fl-left ">
-                Soft/Hard Cap
+          <div className="contain-right fl-right">
+            <div className="contain2">
+              <div className="contain2_section1 clear">
+                <div className="contain2_section1_sub1 fl-left ">
+                  Soft/Hard Cap
+                </div>
+                <div className="contain2_section1_sub2 fl-right">Live</div>
               </div>
-              <div className="contain2_section1_sub2 fl-right">Live</div>
-            </div>
-            {/* section1 */}
-            <div className="contain2_section2 clear">
-              <div className="contain2_section2_sub1 fl-left">
-                <img src="/img/RIP.png" alt="" srcset="" />
+              {/* section1 */}
+              <div className="contain2_section2 clear">
+                <div className="contain2_section2_sub1 fl-left">
+                  <img src="/img/RIP.png" alt="" srcset="" />
+                </div>
+                <div className="contain2_section2_sub2 fl-left">240,000</div>
               </div>
-              <div className="contain2_section2_sub2 fl-left">240,000</div>
-            </div>
-            {/* section 2 */}
-            <div className="contain2_section3 clear">
-              <div className="contain2_section3_sub1 pg30 fl-left">
-                Min Allocation
-              </div>
-              <div className="contain2_section3_sub2 pg30 fl-right">
-                100 RBA
-              </div>
-              <div className="clear"></div>
-              <div className="contain2_section3_sub1 pg20 fl-left">
-                Max Allocation
-              </div>
-              <div className="contain2_section3_sub2 pg20 fl-right">
-                10,000 RBA
-              </div>
-              <div className="clear"></div>
-              <div className="contain2_section3_sub1_sm1 pg30 fl-left">
-                120,000 RBA
-              </div>
-              <div className="contain2_section3_sub2_sm2 pg30 fl-right">
-                240,000 RBA
-              </div>
-              <div className="contain2_section3_bar fl-left">
-                <div className="box3 clear">
-                  <div class="w3-light-grey">
-                    <div class="w3-container w3-green w3-center">25%</div>
+              {/* section 2 */}
+              <div className="contain2_section3 clear">
+                <div className="contain2_section3_sub1 pg30 fl-left">
+                  Min Allocation
+                </div>
+                <div className="contain2_section3_sub2 pg30 fl-right">
+                  100 RBA
+                </div>
+                <div className="clear"></div>
+                <div className="contain2_section3_sub1 pg20 fl-left">
+                  Max Allocation
+                </div>
+                <div className="contain2_section3_sub2 pg20 fl-right">
+                  10,000 RBA
+                </div>
+                <div className="clear"></div>
+                <div className="contain2_section3_sub1_sm1 pg30 fl-left">
+                  120,000 RBA
+                </div>
+                <div className="contain2_section3_sub2_sm2 pg30 fl-right">
+                  240,000 RBA
+                </div>
+                <div className="contain2_section3_bar fl-left">
+                  <div className="box3 clear">
+                    <div class="w3-light-grey">
+                      <div class="w3-container w3-green w3-center">25%</div>
+                    </div>
+                    <br />
                   </div>
-                  <br />
                 </div>
               </div>
-            </div>
-            {/* section 3 */}
-            <div className="contain2_section4 clear">
-              <div className="contain2_section4_middle">
-                <button>Join Sale</button>
+              {/* section 3 */}
+              <div className="contain2_section4 clear">
+                <div className="contain2_section4_middle">
+                  <button></button>
+                </div>
               </div>
-            </div>
-            {/* section 4 */}
-            <div className="contain2_section5 clear">
+              {/* section 4 */}
+              {/* <div className="contain2_section5 clear">
               <div className="contain_section5_middle">Sale Ends in</div>
               <div className="contain_section5_timer_day fl-left">00</div>
               <div className="contain_section5_timer_dot">:</div>
@@ -322,11 +323,51 @@ class Home extends React.Component {
               <div className="contain_section5_timer_day fl-left">21</div>
               <div className="contain_section5_timer_dot">:</div>
               <div className="contain_section5_timer_day fl-left">38</div>
+            </div> */}
+            </div>
+            <div className="clear"></div>
+
+            {/* content3 */}
+            <div className="contain2">
+              <div className="contain3_section_heading clear">
+                More Details
+                <hr />
+              </div>
+              <div className="contain3_section_inner_block clear">
+                <div className="contain3_section_subpart1 pd-20px fl-left">
+                  Amount Allocated
+                </div>
+                <div className="contain3_section_subpart2 pd-20px fl-right">
+                  5,000 RBA
+                </div>
+                <hr />
+              </div>
+              <div className="contain3_section_inner_block clear">
+                <div className="contain3_section_subpart1 pd-20px fl-left">
+                  Amount Recieved
+                </div>
+                <div className="contain3_section_subpart2 pd-20px fl-right">
+                  411,285 SXP
+                </div>
+                <hr />
+              </div>
+              <div className="contain3_section_inner_block clear">
+                <div className="contain3_section_subpart1 pd-20px fl-left">
+                  Claimable Now
+                </div>
+                <div className="contain3_section_subpart2 pd-20px fl-right">
+                  4,285 SXP
+                </div>
+                <hr />
+              </div>
+              <div className="contain3_section clear">
+                <div className="contain3_section_middle">
+                  <button>Claim</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-
-        <div className="clear"></div>
       </div>
     );
   }
