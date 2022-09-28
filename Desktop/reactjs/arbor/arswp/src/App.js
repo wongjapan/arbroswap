94bf4f61fae1f8427949dee0bce716ca29877469
samshals19 <56256306+samshals19@users.noreply.github.com>
2022-09-28 11:58:49 +0530

    smch

--- Desktop/reactjs/arbor/arswp/src/App.js
+++ Desktop/reactjs/arbor/arswp/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-// import LaunchpadPools1 from "./LaunchpadPools1ALive";
+import LaunchpadPools1 from "./LaunchpadPools1ALive";
 // import LaunchpadPools1 from "./AirdropperAirdrop8";
 // import LaunchpadPools1 from "./AirdropperAirdrop9";
 // import LaunchpadPools1 from "./ModalPOPUP";
@@ -7,7 +7,7 @@ import "./App.css";
 // import LaunchpadPools1 from "./LaunchpadPools10";
 // import LaunchpadPools1 from "./AdminMode/StandardP4";
 // import LaunchpadPools1 from "./AdminMode/FairlaunchP4";
-import LaunchpadPools1 from "./AdminMode/PrivateP4";
+// import LaunchpadPools1 from "./AdminMode/PrivateP4";
 import LaunchpadPools3 from "./LaunchpadPools3";
 import LaunchpadPools4 from "./LaunchpadPools4";
 import LaunchpadPools5 from "./LaunchpadPools5";
